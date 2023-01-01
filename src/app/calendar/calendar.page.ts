9ee98b51ad59d0812df5d70b372a8a90ee816689
Yenebeb Daniel Fikre <r0901225@student.thomasmore.be>
2023-01-01 20:47:46 +0100

    remove comments

--- src/app/calendar/calendar.page.ts
+++ src/app/calendar/calendar.page.ts
@@ -14,37 +14,6 @@ export class CalendarPage implements OnInit {
               public authService: FireAuthService,) {
   }
 
-  async ngOnInit(): Promise<void> {
-    // await this.fireStoreService.getAllRoutineWorkoutsGroupedByWeek()
-    //   .then(x=> this.groupedWorkoutSubs = x.subscribe( y => {
-    //     const [key, value]= y;
-    //     this.groupedWorkouts.push(value);
-    //   })).then(()=> {
-    //     this.groupedWorkouts.slice(1);
-    //     console.log(this.groupedWorkouts);
-    //   });
-   // const observableGroupedWorkouts  = this.fireStoreService.getAllRoutineWorkoutsGroupedByWeek();
-   //   this.groupedWorkoutSubs = observableGroupedWorkouts
-   //     .pipe(
-   //       map(w => w.map(wo => wo.startWorkoutTimeStamp)),
-   //       groupBy(startDate => moment(startDate.))
-   //     ).subscribe();
-    // await this.fireStoreService.getAllRoutineWorkoutsGroupedByWeek()
-    //   .then(x=> this.groupedWorkoutSubs = x.subscribe( y => {
-    //     const [key, value]= y;
-    //     this.groupedWorkouts.push(value);
-    //   })).then(()=> {
-    //     this.groupedWorkouts.slice(1);
-    //     console.log(this.groupedWorkouts);
-    //   });
-  // await firstValueFrom(this.fireStoreService.getAllRoutineWorkoutsGroupedByWeek())
-  //   .then(x => {
-  //     const source=from(x);
-  //     return source.pipe(
-  //       groupBy(workout => moment(workout.startWorkoutTimeStamp.toDate()).week()),
-  //       mergeMap(group=> zip(of(group.key), group.pipe(toArray())))
-  //     );
-  //   }).then(x => x.subscribe(y => console.log(y)));
+  ngOnInit(): void {
   }
-
 }
